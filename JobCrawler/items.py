7526fd22b4b5d118cad49380442d4a456ddbf694
root <root@localhost.localdomain>
2018-07-16 21:35:22 +0800

    UPDATE: crawl streagy

--- JobCrawler/items.py
+++ JobCrawler/items.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 
 import scrapy
@@ -39,4 +40,47 @@ class EntranceItem(scrapy.Item):
     # 搜索关键词
     key_word = scrapy.Field()
     # 所属领域
+=======
+# -*- coding: utf-8 -*-
+
+import scrapy
+from scrapy.contrib.loader import ItemLoader
+from scrapy.contrib.loader.processor import TakeFirst, Join, MapCompose
+
+
+class JobcrawlerItem(scrapy.Item):
+    # 职位ID
+    job_id = scrapy.Field()
+    # 职位名
+    job_name = scrapy.Field()
+    # 公司名
+    company = scrapy.Field()
+    # 工作地点
+    job_city = scrapy.Field()
+    # 区域
+    area = scrapy.Field()
+    # 工资
+    salary = scrapy.Field()
+    # 发布时间
+    create_time = scrapy.Field()
+    # 底薪
+    salary_min = scrapy.Field()
+    # 最高薪资
+    salary_max = scrapy.Field()
+    # 平均工资
+    salary_avg = scrapy.Field()
+    # 为了便于分类, 引入搜索关键词
+    key_word = scrapy.Field()
+    
+
+class JobItemLoader(ItemLoader):
+    default_output_processor = TakeFirst()
+    review_in = MapCompose(lambda x: x.replace("\n", " "))
+    review_out = Join()
+
+class EntranceItem(scrapy.Item):
+    # 搜索关键词
+    key_word = scrapy.Field()
+    # 所属领域
+>>>>>>> eb4c479ef153d37e72fa8f0c6d0e3e771a57108e
     key_field = scrapy.Field()
--- JobCrawler/spiders/spider.py
+++ JobCrawler/spiders/spider.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # coding=utf-8
 import re
 import json
@@ -77,4 +78,85 @@ class JobSpider(Spider):
         if next_url:
             # 注意extract是list，所以要转化为str
             next_url = ''.join(next_url)
+=======
+# coding=utf-8
+import re
+import json
+from scrapy import Request
+from scrapy.spiders import Spider
+# 下面的import是正确的，但在pycharm中会有红色警告横线，改用..items
+# from douban.items import DoubanMovieItem, JobItem
+from ..items import JobcrawlerItem, JobItemLoader
+import pandas as pd 
+
+
+class JobSpider(Spider):
+    name = 'jobCrawler'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
+    }
+
+    def start_requests(self):
+
+        file_csv = pd.read_csv('field.csv')
+        data = []
+        for i in file_csv.values:
+            # 把DataFrame格式转为list
+            data.append(i[1])
+
+        area_top = {'010000': '北京', '020000': '上海', '030200': '广州', '040000': '深圳', \
+        '200200': '西安', '180200': '武汉', '080200': '杭州', '070200': '南京', '090200': '成都',\
+        '060000': '重庆', '230200': '沈阳', '120300': '青岛', '080300': '宁波', '170200': '郑州', \
+        '050000': '天津', '070300': '苏州', '190200': '长沙', '070400': '无锡', '030800': '东莞', '01': '珠三角'}
+
+        area_top.keys()
+
+        # 珠三角地区 惠州、汕头、珠海、佛山、中山、江门、湛江、肇庆、清远
+        #area_zhusanjiao = 01
+
+        # while(1):
+        #     area = input('请选择职位地区数字：1.全国， 2.东莞')
+
+        #     if(str(area) != '1' and area != '2'):
+        #         print("输入有误，请重新输入")
+        #     else:
+        #         break
+        allowed_domain = ['http://search.51job.com']
+        for keyword in data:
+            # if area == '1':
+            #     url = 'http://search.51job.com/jobsearch/search_result.php?fromJs=1&keyword=%s' \
+            #    '&keywordtype=2&lang=c&stype=2&postchannel=0000&fromType=1&confirmdate=9' % (keyword)
+            # if(area == '2'):
+            #     url = 'http://search.51job.com/jobsearch/search_result.php?fromJs=1&jobarea=030800&keyword=%s' \
+            #    '&keywordtype=2&lang=c&stype=2&postchannel=0000&fromType=1&confirmdate=9' % (keyword)
+            for area in area_top.keys():
+                url = 'http://search.51job.com/jobsearch/search_result.php?fromJs=1&jobarea=%s&keyword=%s' \
+                '&keywordtype=2&lang=c&stype=2&postchannel=0000&fromType=1&confirmdate=9' % (area, keyword)
+                # 使用meta传递key_word搜索关键词
+                yield Request(url, headers=self.headers, meta={'key_word': keyword, 'area': area_top[area]})   
+
+
+    def parse(self, response):
+        item = JobcrawlerItem()
+        jobs = response.xpath('//*[@id="resultList"]/div[@class="el"]')
+        
+        for job in jobs:
+            loader = JobItemLoader(item=JobcrawlerItem(), selector=job)
+            item['key_word'] = response.meta['key_word']
+            item['job_id'] = job.xpath('.//p/input/@value').extract()
+            # strip去除前后空格,extract的结果是list，用join结合为字符串再进行strip，避免中文乱码
+            job_name = job.xpath('.//p/span/a/text()').extract()
+            item['job_name'] = ''.join(job_name).strip()
+            item['company'] = job.xpath('.//span[@class="t2"]/a/text()').extract()
+            item['job_city'] = job.xpath('.//span[@class="t3"]/text()').extract()
+            item['area'] = response.meta['area']
+            item['salary'] = job.xpath('.//span[@class="t4"]/text()').extract()
+            item['create_time'] = job.xpath('.//span[@class="t5"]/text()').extract()
+            yield item
+        
+        next_url = response.xpath('//div[@class="p_in"]/ul/li[8]/a/@href').extract()
+        if next_url:
+            # 注意extract是list，所以要转化为str
+            next_url = ''.join(next_url)
+>>>>>>> eb4c479ef153d37e72fa8f0c6d0e3e771a57108e
             yield Request(next_url, headers=self.headers, meta={'key_word': response.meta['key_word']})
